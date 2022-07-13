87d9ca1b18284c4993edb561442bbd3340dfbfa2
Vince <vincenthai@gmail.com>
2022-07-13 16:19:03 -0700

    jwt auth

--- backend/server.js
+++ backend/server.js
@@ -18,5 +18,7 @@ app.use(errorHandler);
 
 // Routes for /api/goals
 app.use('/api/goals', require('./routes/goalRoutes'));
+// Routes for users
+app.use('/api/users/', require('./routes/userRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
