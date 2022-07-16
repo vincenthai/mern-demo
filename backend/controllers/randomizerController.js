e6499fe056d2e0dfca352b7643473b0ec7963641
Vince <vincenthai@gmail.com>
2022-07-16 00:02:44 -0700

    fix bug

--- backend/controllers/randomizerController.js
+++ backend/controllers/randomizerController.js
@@ -22,22 +22,24 @@ const lightOrDark = (color) => {
     return hsp > 127.5 ? 'light' : 'dark';
 };
 
-const getRandomNailPolish = asyncHandler(async (type) => {
+const getRandomNailPolish = asyncHandler(async (type, user) => {
     let limit = 0;
     let foundPolish = false;
     let nailPolish;
+
+    const myNailPolishes = await NailPolish.find({ user: user._id });
     // keep trying to find the polish if not found
     while (!foundPolish && limit < 10) {
-        nailPolish = await NailPolish.aggregate([{ $sample: { size: 1 } }]);
-        foundPolish = lightOrDark(nailPolish[0].color) === type;
+        nailPolish = myNailPolishes[Math.floor(Math.random() * myNailPolishes.length)];
+        foundPolish = lightOrDark(nailPolish.color) === type;
         limit++;
     }
-    return nailPolish[0];
+    return nailPolish;
 });
 
-const getRandomPattern = asyncHandler(async () => {
-    const pattern = await Pattern.aggregate([{ $sample: { size: 1 } }]);
-    return pattern[0];
+const getRandomPattern = asyncHandler(async (user) => {
+    const myPatterns = await Pattern.find({ user: user._id });
+    return myPatterns[Math.floor(Math.random() * myPatterns.length)];
 });
 
 // @desc	Return a random nail polish and pattern combo based on dark/light preferences
@@ -48,8 +50,9 @@ const randomize = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error('Please make sure you selected a type');
 	}
-    const polish = await getRandomNailPolish(req.body.type);
-    const pattern = await getRandomPattern();
+
+    const polish = await getRandomNailPolish(req.body.type, req.user);
+    const pattern = await getRandomPattern(req.user);
     res.status(200).json([polish, pattern]);
 });
 
